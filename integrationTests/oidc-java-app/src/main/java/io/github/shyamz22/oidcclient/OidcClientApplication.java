760f0ee5ac17edecf06537ced3e7a0395d826c3b
shyamz-22 <shykart2203@gmail.com>
2018-02-10 14:45:48 +0100

    Add integration test stub

---
+++ integrationTests/oidc-java-app/src/main/java/io/github/shyamz22/oidcclient/OidcClientApplication.java
@@ -0,0 +1,12 @@
+package io.github.shyamz22.oidcclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OidcClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OidcClientApplication.class, args);
+	}
+}
---
+++ integrationTests/oidc-java-app/src/test/java/io/github/shyamz22/oidcclient/OidcClientApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.shyamz22.oidcclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OidcClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
